2ee1c30b2db02f0217ef16d96fefbf9c3270e88e
UnicornChance <chance@defenseunicorns.com>
2024-09-08 15:14:45 -0600

    init

---
+++ src/config.js
@@ -0,0 +1,3 @@
+// src/config.js
+export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+export const REPO_URL = process.env.REPO_URL;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+require('dotenv').config(); // Load .env file variables into process.env
 
 module.exports = {
   entry: './src/bot.js',
@@ -7,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bot.bundle.js',
   },
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -15,19 +16,29 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
         },
       },
     ],
   },
   plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: '' }, // Copy static assets
-        { from: 'src/worker.js', to: '' }, // Copy worker.js to the dist directory
-      ],
+    new webpack.DefinePlugin({
+      'process.env.GITHUB_TOKEN': JSON.stringify(process.env.GITHUB_TOKEN),
+      'process.env.REPO_URL': JSON.stringify(process.env.REPO_URL),
     }),
   ],
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      crypto: require.resolve('crypto-browserify'),
+      vm: require.resolve('vm-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+    },
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000,
+  },
 };
